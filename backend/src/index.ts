9248e0bd3e9d2b06a8b94e839add0af68744c6a3
pu5hp <pushpdeepsinghv@gmail.com>
2024-03-12 22:48:06 +0530

    added sign and signup routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,25 @@
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono'
+import { decode, sign, verify } from 'hono/jwt'
 //important in serverless environment variables the routes may be independently deployed. 
 //bindings here is used for the c.env.DATABASE_URL type definition
+
+
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string
+    DATABASE_URL: string,
+    JWT_SECRET:string
   }
 }>();
+
+app.use('/api/v1/blog/*', async (c, next) => {
+  //get the header
+  //verify the header
+  //if the header is correct,we can proceed
+  //if not return the user a 403 status code
+  await next()
+})
 
 //sign-up route
 app.post('/api/v1/user/signup',async (c)=>{
@@ -16,18 +28,63 @@ app.post('/api/v1/user/signup',async (c)=>{
 }).$extends(withAccelerate())
 
 const body = await c.req.json();
-const encryptPassword = new TextEncoder().encode(body.password);
-await prisma.user.create({
+
+const BeforeHash = new TextEncoder().encode(body.password);//in certain format before hashing
+const hashedPassword = await crypto.subtle.digest(
+  {
+    name: 'SHA-256',
+  },
+  BeforeHash // The data you want to hash as an ArrayBuffer
+);
+
+var pass = new TextDecoder().decode(hashedPassword);
+
+//returns object
+const user = await prisma.user.create({
   data:{
     email: body.email,
-    password: body.password,
+    password: pass,
   }
 })
-  return c.json('signup');
+
+const token = await sign({id:user.id},c.env.JWT_SECRET);
+  return c.json({ jwt:token});
 })
 
-app.post('/api/v1/user/signin',(c)=>{
-  return c.json('signin');
+
+
+//signin route
+app.post('/api/v1/user/signin',async (c)=>{
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+
+const body = await c.req.json();
+
+const BeforeHash = new TextEncoder().encode(body.password);//in certain format before hashing
+const hashedPassword = await crypto.subtle.digest(
+  {
+    name: 'SHA-256',
+  },
+  BeforeHash // The data you want to hash as an ArrayBuffer
+);
+
+var pass = new TextDecoder().decode(hashedPassword);
+//check if the password is same as in user db
+var user = await prisma.user.findUnique({
+  where: {
+    email: body.email,
+    password:pass
+  }
+});
+
+if(user===null){
+  c.status(403);
+  return c.json('User not found')
+}
+const jwt = await sign({id: user.id},c.env.JWT_SECRET);
+  return c.json({jwt});
 })
 
 app.post('/api/v1/blog',(c)=>{
